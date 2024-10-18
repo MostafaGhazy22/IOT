5c23a9cd404eebab0eb82db23f648d306be62ae8
MostafaGhazy22 <Mghazy196@gmail.com>
2024-10-18 16:59:44 +0300

    Delete unused files

--- iot/Core/Inc/uart_program.h
+++
@@ -1,9 +0,0 @@
-#ifndef UART_PROGRAM_H
-#define UART_PROGRAM_H
-
-#include "stm32f4xx_hal.h"
-
-void UART_Init(void);  // Function to initialize UART reception
-uint16_t UART_GetData(uint8_t* dataArray, uint16_t maxSize);  // Function to get the received data
-
-#endif  // UART_PROGRAM_H
